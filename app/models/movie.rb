ed05258722bcdccab39a17fdcb4611e152d7b837
Moh'd Alrefai <malrefai@nyit.edu>
2017-06-30 10:06:19 +0300

    Change stub to mock at spec in movie_controller_spec

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,28 +1,28 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings
-    %w(G PG PG-13 NC-17 R)
-  end
-
-  class Movie::InvalidKeyError < StandardError;
-  end
-
+  # def self.all_ratings
+  #   %w(G PG PG-13 NC-17 R)
+  # end
+  #
+  # class Movie::InvalidKeyError < StandardError;
+  # end
+  #
   # Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
-
-  def self.find_in_tmdb(string)
-    # begin
-    #   if Tmdb::Movie.find(string) != nil
-    #     moviesList=[]
-    #     movieRating = nil
-    #     Tmdb::Movie.find(string).each do |movie|
-    #       movieRating = getRating(movie.id)
-    #       if !movie.release_date.blank?
-    #         moviesList << {:title => movie.title, :rating => movieRating, :tmdb_id => movie.id, :release_date => movie.release_date}
-    #       end
-    #     end
-    #   end
-    #   return moviesList
-    # rescue Tmdb::InvalidApiKeyError
-    #   raise Movie::InvalidKeyError, 'Invalid API key'
-    # end
-  end
+  #
+  # def self.find_in_tmdb(string)
+  #   begin
+  #     if Tmdb::Movie.find(string) != nil
+  #       moviesList=[]
+  #       movieRating = nil
+  #       Tmdb::Movie.find(string).each do |movie|
+  #         movieRating = getRating(movie.id)
+  #         if !movie.release_date.blank?
+  #           moviesList << {:title => movie.title, :rating => movieRating, :tmdb_id => movie.id, :release_date => movie.release_date}
+  #         end
+  #       end
+  #     end
+  #     return moviesList
+  #   rescue Tmdb::InvalidApiKeyError
+  #     raise Movie::InvalidKeyError, 'Invalid API key'
+  #   end
+  # end
 end
--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -5,6 +5,7 @@ describe MoviesController do
 
   describe 'searching TMDb' do
     it 'should call the model method that performs TMDb search' do
+      Movie = double('Movie')
       fake_results = [double('movie1'), double('movie2')]
       expect(Movie).to receive(:find_in_tmdb).with('hardware').and_return(fake_results)
       post :search_tmdb, params: {search_terms: 'hardware'}
